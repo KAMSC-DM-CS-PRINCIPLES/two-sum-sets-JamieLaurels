0f1e78ca4995bbc016e77cef331564cbbfffdfa1
JamieLaurels <03lucas.jude@kamsc.org>
2025-09-16 12:35:08 -0400

    solved two_sum_pairs.py

--- two_sum.py
+++ two_sum.py
@@ -23,8 +23,21 @@ def two_sum_pairs(numbers, target):
     
     # When you are ready with your code make sure to return your list! This is so the autograder can grade!
     # return ExampleListName
+    # set.__hash__ = lambda x: "".join(sorted(x))
+
+    # numbers = set(numbers)
+    answers = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i]+numbers[j] == target:
+                answers.append(set([numbers[i],numbers[j]]))
+    print(list(answers))
+    return answers
 
 # Test cases - you can run this file directly to test your function!
-if __name__ == "__main__":
+two_sum_pairs([1, 2, 3, 4], 5)
 
+
+# if __name__ == "__main__":
+#     two_sum_pairs()
 
